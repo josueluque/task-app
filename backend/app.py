811cde76e7734a9b026fe94417fa8294a3576613
Josue Luque <jluqueherbas@gmail.com>
2024-02-01 17:02:07 -0300

    Added entry point

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from routes.taskRoute import task
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(
+    title="Task App",
+    description="Task App using FARM stack - FastAPI, ReactJS, MongoDB ",
+    version="0.1.0"
+)
+
 
 @app.get('/')
 def welcome():
     return {"message": "Welcome to my FastAPI project"}
 
 app.include_router(task)
-    
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=8000, log_level="info", reload=True)
