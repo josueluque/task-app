2975a4c848bef909c805ba76346ff32b95ef112b
Josue Luque <jluqueherbas@gmail.com>
2024-02-05 13:46:07 -0300

    Added more enpoints in taskRoute and functions async in taskService

--- backend/routes/taskRoute.py
+++ backend/routes/taskRoute.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from models.taskModel import Task
+from models.taskModel import Task, UpdateTask
 from services import taskService
 
 
@@ -10,27 +10,42 @@ task = APIRouter()
 
 @task.get('/task/tasks')
 async def get_tasks():
-    tasks = await taskService.get_tasks()
+    tasks = await taskService.get_all_tasks()
     return tasks
 
-@task.get('/task/getTask/{id}')
-async def get_task(id: int):
-    return {"data": id}
 
-@task.post('/task/createTask')
+@task.get('/task/getTask/{id}', response_model=Task)
+async def get_task(id: str):
+    task = await taskService.get_task_by_id(id)
+    if task:
+        return task
+    raise HTTPException(404, f"Task with id {id} not found")    
+
+
+@task.post('/task/createTask', response_model=Task)
 async def create_task(task: Task):
-    response = await taskService.add_new_task(task.dict())
+    task_found = await taskService.get_task_by_title(task.title)
+    if task_found:
+        raise HTTPException(409, "Task already exists")
+
+    response = await taskService.create_task(task.dict())
+    # print(response)
     if response:
         return response
-    raise HTTPException(400, "error")
-    
-    # print(task)
-    # return {"data": "Task created"}
+    raise HTTPException(404, f"Could not create the task")
 
-@task.put('/task/editTask/{id}')
-async def update_task(id: int, data):
-    return {"data": f"Task {id} has been updated"}
 
-@task.delete('/task/deleteTask/{id}')
-async def delete_task(id: int):
-    return {"data": f"Task {id} has been deleted"}
+@task.put('/task/updateTask/{id}', response_model=Task)
+async def update_task(id: str, task: UpdateTask):
+    response = await taskService.update_task_by_id(id, task)
+    if response:
+        return response
+    raise HTTPException(404, f"Task with id {id} not found")
+
+
+@task.delete('/task/deleteTask/{id}')   
+async def remove_task(id: str):
+    response = await taskService.delete_task_by_id(id)
+    if response:
+        return f"Successfull task {id} has been deleted"
+    raise HTTPException(404, f"Task with id {id} not found")
--- backend/services/taskService.py
+++ backend/services/taskService.py
@@ -1,18 +1,43 @@
 from db.db import collection
 from models.taskModel import Task
+from bson import ObjectId
 
 
 #TODO: Agregar las funciones de consulta y agregar docstrings
 
-async def get_tasks():
+
+async def get_task_by_id(id):
+    task = await collection.find_one({"_id": ObjectId(id)})
+    return task
+
+
+async def create_task(task):
+    new_task = await collection.insert_one(task)
+    created_task = await collection.find_one({"_id": new_task.inserted_id})
+    return created_task
+
+
+async def get_task_by_title(title):
+    task = await collection.find_one({"title": title})
+    return task
+
+
+async def get_all_tasks():
     tasks = []
     cursor = collection.find({})
     async for document in cursor:
         tasks.append(Task(**document))
-
     return tasks
 
-async def add_new_task(task):
-    new_task = await collection.insert_one(task)
-    create_task = await collection.find_one({'_id': new_task.inserted_id})
-    return create_task
+
+async def update_task_by_id(id: str, data):
+    task = {key: value for key, value in data.dict().items() if value is not None}
+    print(task)
+    await collection.update_one({"_id": ObjectId(id)}, {"$set": task})
+    document = await collection.find_one({"_id": ObjectId(id)})
+    return document
+
+
+async def delete_task_by_id(id):
+    await collection.delete_one({"_id": ObjectId(id)})
+    return True
