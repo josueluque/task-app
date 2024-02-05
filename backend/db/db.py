7de4547c16d7adf2d20f2d60621718937668e818
Josue Luque <jluqueherbas@gmail.com>
2024-02-05 13:41:01 -0300

    Changes in taskModel

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-    
-client = AsyncIOMotorClient('mongodb://localhost:27017')
+
+client = AsyncIOMotorClient("mongodb://localhost:27017")
 database = client.taskdb
 collection = database.tasks
