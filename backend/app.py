7080e99bd67b2cba4f9d94cd27425ecf3a11bbaf
Josue Luque <jluqueherbas@gmail.com>
2024-03-03 17:19:51 -0300

    Added middleware

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from routes.taskRoute import task
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config # Modulo de python-decouple
 import uvicorn
 
+# Creación de la aplicación FastAPI
 app = FastAPI(
     title="Task App",
     description="Task App using FARM stack - FastAPI, ReactJS, MongoDB ",
@@ -13,7 +16,23 @@ app = FastAPI(
 def welcome():
     return {"message": "Welcome to my FastAPI project"}
 
+# Definicion de origins permitidos para establecer conexiones
+origins = [
+    config("FRONTEND_URL"),
+]
+
+# Definicion de middleware para manejar solicitudes de recursos cruzados (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+# Inclusión de rutas adicionales desde el conjunto de rutas 'task'
 app.include_router(task)
 
+# Ejecución del servidor Uvicorn
 if __name__ == "__main__":
     uvicorn.run("app:app", port=8000, log_level="info", reload=True)
