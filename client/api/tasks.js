f3facb5b442b67aaf636fc54fb47ff2f8a43d944
Josue <jluqueherbas@gmail.com>
2024-04-16 14:24:54 -0300

    configured axios baseURL for all requests

--- client/api/tasks.js
+++ client/api/tasks.js
@@ -1,14 +1,11 @@
-import axios from "axios";
+import axios from "./axios";
 
-const URL = import.meta.env.VITE_API;
-const endpoint = URL + "/task";
+export const fetchTasks = () => axios.get(`task/tasks`);
 
-export const fetchTasks = () => axios.get(`${endpoint}/tasks`);
+export const fetchTask = (id) => axios.get(`task/getTask/${id}`);
 
-export const fetchTask = (id) => axios.get(`${endpoint}/getTask/${id}`);
+export const createTask = (task) => axios.post(`task/createTask`, task);
 
-export const createTask = (task) => axios.post(`${endpoint}/createTask`, task);
+export const updateTask = (id, task) => axios.put(`task/updateTask/${id}`, task);
 
-export const updateTask = (id, task) => axios.put(`${endpoint}/updateTask/${id}`, task);
-
-export const deleteTask = (id) => axios.delete(`${endpoint}/deleteTask/${id}`);
+export const deleteTask = (id) => axios.delete(`task/deleteTask/${id}`);    
